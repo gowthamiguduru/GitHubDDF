da39730363df5e1b22aa8178d023d76d15d195d5
Sunny <Sunny@LAPTOP-BCBO1JTN>
2024-03-25 14:38:22 +0530

    created new class

---
+++ DataDrivenFramework/src/commonFunction/Sample_Code.java
@@ -0,0 +1,18 @@
+package commonFunction;
+
+public class Sample_Code {
+public void add()
+{
+	int a=10;
+	int b=20;
+	int c=a+b;
+	System.out.println(c);
+	}
+public void divison()
+{
+int a=678;
+int b=123;
+int c=a/b;
+System.out.println(c);
+}
+}
