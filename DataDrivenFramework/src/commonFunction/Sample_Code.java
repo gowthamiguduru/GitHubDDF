2bfcd2a772f94fc33945c373b0b090c646bb28f5
Sunny <Sunny@LAPTOP-BCBO1JTN>
2024-03-25 14:50:59 +0530

    modified class

--- DataDrivenFramework/src/commonFunction/Sample_Code.java
+++ DataDrivenFramework/src/commonFunction/Sample_Code.java
@@ -3,14 +3,14 @@ package commonFunction;
 public class Sample_Code {
 public void add()
 {
-	int a=10;
-	int b=20;
+	int a=100;
+	int b=200;
 	int c=a+b;
 	System.out.println(c);
 	}
 public void divison()
 {
-int a=678;
+int a=6786;
 int b=123;
 int c=a/b;
 System.out.println(c);
